0e944ab30e61872a13786bd0e2326627cb29b6a6
khalilgreenidge <khalilgreenidge16@gmail.com>
2023-01-30 22:12:20 +0000

    init

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,6 +4,8 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +21,21 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete descriptnion, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The tracks's complete array of modules"
+    modules: [Module!]!
+  }
+
+  "A module is a single unit of teaching. Multiple modules compose a track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in minutes"
+    length: Int
   }
 
   "Author of a complete Track or a Module"
