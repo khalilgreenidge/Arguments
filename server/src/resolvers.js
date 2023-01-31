28bdaf3fe74ca48d0507aeb4278cb84cef0b2f2d
khalilgreenidge <khalilgreenidge16@gmail.com>
2023-01-31 06:23:53 +0000

    updates

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,12 +4,19 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // get a single track by id, for the track page
+    track: (_, {id}, {dataSources}) => { 
+      dataSources.trackAPI.getTrack(id)
+    }
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
+    author: ({ authorId }, _, { dataSources }) => {  
       return dataSources.trackAPI.getAuthor(authorId);
     },
-  },
+    modules: ({id}, _, { dataSources}) => {
+      return dataSources.trackAPI.getTrackModules(id)
+    }
+  }
 };
 
 module.exports = resolvers;
